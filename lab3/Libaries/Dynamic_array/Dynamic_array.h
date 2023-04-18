ed63fa38191abf0ae5839a07cb345b954491bab5
Almairo <53612796+Almir228@users.noreply.github.com>
2023-04-18 21:37:57 +0300

    Add files via upload

---
+++ lab3/Libaries/Dynamic_array/Dynamic_array.h
@@ -0,0 +1,20 @@
+#pragma
+#include <iostream>
+
+
+struct DynamicArray {
+    int *value;
+    int size;
+};
+
+DynamicArray create_array(int size);
+void delete_array(DynamicArray arr);
+void delete_ell(DynamicArray *arr, int index);
+void print_array(DynamicArray arr);
+DynamicArray resize_x_2(DynamicArray arr);
+int get_value(DynamicArray arr, int index);
+void push_back(DynamicArray* arr, int value, DynamicArray (*func)(DynamicArray arr));
+void set_value (DynamicArray* arr, int index, int value, DynamicArray (*func)(DynamicArray arr));
+DynamicArray fill_array(int size, int step);
+DynamicArray resize_const(DynamicArray arr);
+DynamicArray resize_plus_1(DynamicArray arr);
