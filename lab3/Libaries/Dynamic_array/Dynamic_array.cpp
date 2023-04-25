53f61b48e614a4d64c11bb8c607571f7db7d3c54
Almairo <53612796+Almir228@users.noreply.github.com>
2023-04-25 13:16:15 +0300

    Add files via upload

--- lab3/Libaries/Dynamic_array/Dynamic_array.cpp
+++ lab3/Libaries/Dynamic_array/Dynamic_array.cpp
@@ -3,15 +3,16 @@
 
 struct DynamicArray{
     int* value;
+    int capasity;
     int size;
     int step;
 };
 
 
-DynamicArray create_array(int size) {
+DynamicArray create_array(int capasity) {
     DynamicArray arr{};
-    arr.value = new int[size];
-    arr.size = size;
+    arr.value = new int[capasity];
+    arr.capasity = capasity;
     return arr;
 }
 
@@ -19,7 +20,8 @@ DynamicArray create_array(int size) {
 DynamicArray fill_array(int size, int step) {
     DynamicArray arr{};
     arr.value = new int[size];
-    arr.size = size;
+    arr.capasity = size;
+
     arr.step = step;
     return arr;
 }
@@ -31,10 +33,10 @@ void delete_array(DynamicArray arr) {
 
 
 void delete_ell(DynamicArray *arr, int index){
-    for (int i = index; i < arr->size-1; i++) {
+    for (int i = index; i < arr->capasity-1; i++) {
         arr->value[i] = arr->value[i + 1];
     }
-    --arr->size;
+    --arr->capasity;
 }
 
 void print_array(DynamicArray arr){
@@ -47,10 +49,11 @@ void print_array(DynamicArray arr){
 
 DynamicArray resize_x_2(DynamicArray arr) {
     DynamicArray newArr{};
-    int newSize = arr.size*2;
-    newArr.value = new int[newSize];
-    newArr.size = newSize;
-    for (int i = 0; i < arr.size; i++) {
+    int newCap = arr.capasity * 2;
+    newArr.size = arr.size;
+    newArr.value = new int[newCap];
+    newArr.capasity = newCap;
+    for (int i = 0; i < arr.capasity; i++) {
         newArr.value[i] = arr.value[i];
     }
     delete[] arr.value;
@@ -59,10 +62,11 @@ DynamicArray resize_x_2(DynamicArray arr) {
 
 DynamicArray resize_plus_1(DynamicArray arr) {
     DynamicArray newArr{};
-    int newSize = arr.size + 1;
+    int newSize = arr.capasity + 1;
+    newArr.size = arr.size;
     newArr.value = new int[newSize];
-    newArr.size = newSize;
-    for (int i = 0; i < arr.size; i++) {
+    newArr.capasity = newSize;
+    for (int i = 0; i < arr.capasity; i++) {
         newArr.value[i] = arr.value[i];
     }
     delete[] arr.value;
@@ -71,11 +75,12 @@ DynamicArray resize_plus_1(DynamicArray arr) {
 
 DynamicArray resize_const(DynamicArray arr) {
     DynamicArray newArr{};
-    int newSize = arr.size+arr.step;
+    int newSize = arr.capasity+arr.step;
     newArr.value = new int[newSize];
-    newArr.size = newSize;
+    newArr.size = arr.size;
+    newArr.capasity = newSize;
     newArr.step = arr.step;
-    for (int i = 0; i < arr.size; i++) {
+    for (int i = 0; i < arr.capasity; i++) {
         newArr.value[i] = arr.value[i];
     }
     delete[] arr.value;
@@ -83,24 +88,26 @@ DynamicArray resize_const(DynamicArray arr) {
 }
 
 int get_value(DynamicArray arr, int index) {
-    if (index >= 0 && index < arr.size) {
+    if (index >= 0 && index < arr.capasity) {
         return arr.value[index];
     }
     return -1;
 }
 
-void push_back(DynamicArray* arr, int value, DynamicArray (*func)(DynamicArray arr)){
+void push_back(DynamicArray* arr, int value, DynamicArray (*func)(DynamicArray arr) = resize_x_2){
     int size = arr->size;
-    *arr = func(*arr);
+    if(arr->size == arr->capasity)
+        *arr = func(*arr);
     arr->value[size] = value;
+    arr->size++;
 }
 
 void set_value (DynamicArray* arr, int index, int value,
-                DynamicArray (*func)(DynamicArray arr)){
+                DynamicArray (*func)(DynamicArray arr) = resize_x_2){
     if (index >= 0 && index < arr->size) {
         arr->value[index] = value;
     }
-    else if (index >= 0 && index == arr->size) {
+    else if (index == arr->size) {
         push_back(arr, value, func);
     }
     else{
